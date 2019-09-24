af34a14da68d30c351bc7b7e3947843bd8595689
Adrienne <adrienneeaston@gmail.com>
2019-09-23 21:00:56 -0700

    Updates

---
+++ precache-manifest.05a2db3a8d1672f7653ef2ba827f4fbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "965723b0481e2754fa8dac4abf41ec89",
+    "url": "/lab32-custom-hook-and-magical-todos/index.html"
+  },
+  {
+    "revision": "79a94970c0924ff96613",
+    "url": "/lab32-custom-hook-and-magical-todos/static/css/main.6389a10d.chunk.css"
+  },
+  {
+    "revision": "3e8a17271a2ab8d47d0e",
+    "url": "/lab32-custom-hook-and-magical-todos/static/js/2.2e61ad13.chunk.js"
+  },
+  {
+    "revision": "79a94970c0924ff96613",
+    "url": "/lab32-custom-hook-and-magical-todos/static/js/main.7b7c5cd2.chunk.js"
+  },
+  {
+    "revision": "ccc6271416f79904d869",
+    "url": "/lab32-custom-hook-and-magical-todos/static/js/runtime-main.dc26d917.js"
+  }
+]);
